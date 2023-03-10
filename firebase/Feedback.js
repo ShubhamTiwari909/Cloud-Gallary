89c3cd74b2dd9ceb6ba670ed5ee39c51a44b5f38
Shubham Tiwari <shubhmtiwri00@gmail.com>
2023-03-10 13:05:17 +0530

    Added a feedback form

--- firebase/Feedback.js
+++ firebase/Feedback.js
@@ -4,8 +4,8 @@ import { database } from "./firebaseConfig";
 export const addFeedbackToDB = (username, setUsername, email, setEmail, message, setMessage) => {
     let curr = new Date();
     curr.setDate(curr.getDate());
-    let date = curr.toISOString().substring(0, 10);
-    const databaseRef = collection(database, `/Gallary/Feedbacks/feebacks`)
+    let date = curr.toISOString();
+    const databaseRef = collection(database, `/Gallary/Feedbacks/feedbacks`)
     addDoc(databaseRef, {
         feedbackId: Math.floor(Math.random() * 9999999),
         userId:sessionStorage.getItem("uid"),
