c49ea4177b9683e68efeecbcd992baf5df95c8c6
Shubham Tiwari <shubhmtiwri00@gmail.com>
2023-06-21 14:43:57 +0530

    fix: overlay issue fixed

--- components/molecules/Details.js
+++ components/molecules/Details.js
@@ -1,33 +1,31 @@
-import { RxCross1 } from "react-icons/rx"
-import Para from "../atoms/text/Para"
-import Button from "../atoms/Button"
+import { RxCross1 } from "react-icons/rx";
+import Para from "../atoms/text/Para";
+import Button from "../atoms/Button";
 
 function Details({ details, detailPopup, setDetailPopup, setOverlay }) {
+    const handleClosePopup = () => {
+        setDetailPopup(false);
+        setOverlay(false);
+    };
 
     return (
-        <div className={`${detailPopup ? "" : "hidden"} w-72 md:w-96 fixed absolute-center z-105 mt-2 bg-white p-4 rounded-lg`
-        }>
-            {details.map((detail,index) => {
-                return (
-                    <div key={index}>
-                        {detail.button ?
-                            detail.button
-                            :
-                            <Para className='text-sm md:text-lg font-semibold mb-2 text-black'>
-                                {detail.name}: <span className='text-slate-700'>{detail.value}</span>
-                            </Para>
-                        }
-                    </div>
-                )
-            })}
-            <Button className='text-sm absolute top-2 right-2' onClick={() => {
-                setDetailPopup(false)
-                setOverlay(false)
-            }}>
-                <RxCross1 size="16px" color='crimson' />
+        <div className={`w-72 md:w-96 fixed absolute-center z-106 mt-2 bg-white p-4 rounded-lg ${detailPopup ? "" : "hidden"}`}>
+            {details.map((detail, index) => (
+                <div key={index}>
+                    {detail.button ? (
+                        detail.button
+                    ) : (
+                        <Para className="text-sm md:text-lg font-semibold mb-2 text-black">
+                            {detail.name}: <span className="text-slate-700">{detail.value}</span>
+                        </Para>
+                    )}
+                </div>
+            ))}
+            <Button className="text-sm absolute top-2 right-2" onClick={handleClosePopup}>
+                <RxCross1 size="16px" color="crimson" />
             </Button>
-        </div >
-    )
+        </div>
+    );
 }
 
-export default Details
+export default Details;
